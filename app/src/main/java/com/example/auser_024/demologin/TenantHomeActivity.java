38e3f20665f71d2c7b080ee736db75c90e411a8a
MonkeyAbula <soga0128@hotmail.com>
2017-11-09 16:32:04 +0800

    updata

--- app/src/main/java/com/example/auser_024/demologin/TenantHomeActivity.java
+++ app/src/main/java/com/example/auser_024/demologin/TenantHomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.auser_024.demologin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TenantHomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class TenantHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tenant_home);
+    }
+
+    public void home (View v) {
+        finish();
+    }
+
+    public void adduser(View v) {
+        Intent intent = new Intent(TenantHomeActivity.this,AddTenantActivity.class);
+        startActivity(intent);
     }
 }
