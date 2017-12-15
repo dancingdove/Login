a8d556546454f6f494d44ae7e06e295991a6d046
MonkeyAbula <soga0128@hotmail.com>
2017-12-15 14:20:52 +0800

    updata1215

--- app/src/main/java/com/example/auser_024/demologin/AddLandlordActivity.java
+++ app/src/main/java/com/example/auser_024/demologin/AddLandlordActivity.java
@@ -2,6 +2,7 @@ package com.example.auser_024.demologin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddLandlordActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class AddLandlordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_landlord);
+    }
+
+    public void cancel(View v) {
+        finish();
     }
 }
--- app/src/main/java/com/example/auser_024/demologin/AddTenantActivity.java
+++ app/src/main/java/com/example/auser_024/demologin/AddTenantActivity.java
@@ -2,6 +2,7 @@ package com.example.auser_024.demologin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddTenantActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class AddTenantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tenant);
+    }
+
+    public void cancel(View v) {
+        finish();
     }
 }
