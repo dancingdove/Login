df493aebbc07944d198cf86fe33b3135ff57ab1a
MonkeyAbula <soga0128@hotmail.com>
2017-12-15 16:25:43 +0800

    updata1215

--- app/src/main/java/com/example/auser_024/demologin/AddLandlordActivity.java
+++ app/src/main/java/com/example/auser_024/demologin/AddLandlordActivity.java
@@ -1,15 +1,64 @@
 package com.example.auser_024.demologin;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddLandlordActivity extends AppCompatActivity {
+
+    EditText et4,et5,et6,et7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_landlord);
+    }
+
+    public void btnRegister(View v) {
+
+        et4 = (EditText)findViewById(R.id.editText2);
+        et5 = (EditText)findViewById(R.id.editText7);
+        et6 = (EditText)findViewById(R.id.editText5);
+        et7 = (EditText)findViewById(R.id.editText6);
+//        Editable Email = et1.getText();
+//        Editable UserName = et2.getText();
+//        Editable Password = et3.getText();
+//        Editable Age = et4.getText();
+
+        if (!et4.getText().toString().trim().equals("")
+                && !et5.getText().toString().trim().equals("")
+                && !et6.getText().toString().trim().equals("")
+                && !et7.getText().toString().trim().equals("")) {
+
+
+            if (et6.getText().toString().trim().equals(et7.getText().toString().trim())) {
+
+                SharedPreferences settings = getSharedPreferences("create" , 0);
+
+                settings.edit().putString("account" , et4.getText().toString()).commit();
+                settings.edit().putString("email" , et5.getText().toString()).commit();
+                settings.edit().putString("password" , et6.getText().toString()).commit();
+                Toast.makeText(AddLandlordActivity.this, "申請成功", Toast.LENGTH_SHORT).show();
+//            restorePrefs();
+                Intent intent = new Intent();
+                intent.setClass(AddLandlordActivity.this, HomePage.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("account", et5.getText().toString());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }else {
+                Toast.makeText(AddLandlordActivity.this,"密碼不相符...",Toast.LENGTH_LONG).show();
+            }
+
+        }else {
+
+            Toast.makeText(AddLandlordActivity.this,"欄位不可空白",Toast.LENGTH_LONG).show();
+        }
     }
 
     public void cancel(View v) {
--- app/src/main/java/com/example/auser_024/demologin/LandlordHome.java
+++ app/src/main/java/com/example/auser_024/demologin/LandlordHome.java
@@ -1,16 +1,52 @@
 package com.example.auser_024.demologin;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LandlordHome extends AppCompatActivity {
+
+    EditText et1,et2,et3;
+    public static final String PREF = "Login_PREF";
+    public static final String PREF_UserName = "PREF_USERNAME";
+    public static final String PREF_Password = "PREF_PASSWORD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landlord_home);
+        et1 = (EditText)findViewById(R.id.et1);
+        et2 = (EditText)findViewById(R.id.et3);
+        et3 = (EditText)findViewById(R.id.et4);
+    }
+
+    public void signIn(View v) {
+        String lorduser =  et2.getText().toString();
+        String lordmail = et1.getText().toString();
+        String lordpassword = et3.getText().toString();
+        SharedPreferences settings = getSharedPreferences("create" , 0);
+        String account = settings.getString("account", "");
+        String email = settings.getString("email", "");
+        String password = settings.getString("password", "");
+        if (et1.getText().toString().trim().equals("")
+                || et2.getText().toString().trim().equals("")
+                || et3.getText().toString().trim().equals("")) {
+            Toast.makeText(LandlordHome.this,"欄位不可空白",Toast.LENGTH_SHORT).show();
+        }else if ((lorduser.equals(account)) && (lordmail.equals(email)) && (lordpassword.equals(password))) {
+            Toast.makeText(LandlordHome.this,"帳號驗證成功",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent();
+            intent.setClass(LandlordHome.this,HomePage.class);
+//            Bundle bundle = new Bundle();
+//            bundle.putString("account",et2.getText().toString());
+//            intent.putExtras(bundle);
+            startActivity(intent);
+        }else {
+            Toast.makeText(LandlordHome.this,"帳號驗證失敗",Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void home(View v) {
